5aa5b2b50223a658c8424d6dce0fa303816f8c69
Caleb Eggensperger <calebegg@gmail.com>
2012-08-27 13:50:51 -0500

    License

---
+++ src/org/proofpad/package-info.java
@@ -0,0 +1,24 @@
+/**
+ *  Proof Pad: An IDE for ACL2.
+ *  Copyright (C) 2012 Caleb Eggensperger
+ *  <p>
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  <p>
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  <p>
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ *  <p>
+ *  To contact the author, send email to calebegg@gmail.com.
+ *
+ *  @author Caleb Eggensperger
+ *  @version {@value Main.RELEASE}
+ */
+
+package org.proofpad;
