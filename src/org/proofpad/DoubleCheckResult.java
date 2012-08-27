fd39f01d3f1e32861fa5b1744d9d38e38fb60547
Caleb Eggensperger <calebegg@gmail.com>
2012-08-27 13:51:22 -0500

    Stub for DoubleCheck results

---
+++ src/org/proofpad/DoubleCheckResult.java
@@ -0,0 +1,14 @@
+package org.proofpad;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+public class DoubleCheckResult extends JPanel {
+	private static final long serialVersionUID = -2822280272207904268L;
+	
+	public DoubleCheckResult(String output) {
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+	}
+
+}
