013fc7c95eada80c0d47c7c803361e2aeae214ef
Caleb Eggensperger <calebegg@gmail.com>
2012-08-27 13:50:08 -0500

    User data collection

---
+++ src/org/proofpad/UserData.java
@@ -0,0 +1,79 @@
+package org.proofpad;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
+
+public class UserData implements Serializable {
+
+	static class LogUse implements ActionListener {
+		String key;
+		public LogUse(String key) {
+			this.key = key;
+		}
+		@Override public void actionPerformed(ActionEvent e) {
+			Main.userData.addUse(key, e.getModifiers() != 0);
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	private static class Use implements Serializable {
+		private static final long serialVersionUID = 8381796174167421554L;
+		final String key;
+		final boolean keyboard;
+		final Date when;
+		public Use(String key, boolean keyboard, Date when) {
+			this.key = key;
+			this.keyboard = keyboard;
+			this.when = when;
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	private static class ErrorData implements Serializable {
+		private static final long serialVersionUID = 9144659354972366784L;
+		private final String error;
+		private final Date when;
+		private final int release;
+		public ErrorData(String error, Date when, int release) {
+			this.when = when;
+			this.error = error;
+			this.release = release;
+		}
+	}
+
+	private static final long serialVersionUID = -5532381141979625516L;
+	
+	final Date recordingStart = new Date();
+	final UUID uuid = UUID.randomUUID();
+	final String systemData;
+	final ArrayList<Use> uses = new ArrayList<Use>();
+	final ArrayList<ErrorData> errors = new ArrayList<ErrorData>();
+	final ArrayList<ErrorData> parseErrors = new ArrayList<ErrorData>();
+	final ArrayList<ErrorData> replMessages = new ArrayList<ErrorData>();
+
+	
+	public UserData() {
+		systemData = System.getProperty("os.name") + " " + System.getProperty("os.version") + " " +
+				System.getProperty("os.arch");
+	}
+	
+	synchronized void addError(String error) {
+		errors.add(new ErrorData(error, new Date(), Main.RELEASE));
+	}
+	
+	synchronized void addUse(String key, boolean keyboard) {
+		uses.add(new Use(key, keyboard, new Date()));
+	}
+
+	synchronized void addParseError(String msg) {
+		parseErrors.add(new ErrorData(msg, new Date(), Main.RELEASE));		
+	}
+
+	synchronized void addReplMsg(String msg) {
+		replMessages.add(new ErrorData(msg, new Date(), Main.RELEASE));		
+	}
+}
