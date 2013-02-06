931fcda0fceea47aefead5213b60895b97265b40
Caleb Eggensperger <calebegg@gmail.com>
2013-02-06 00:10:33 -0600

    Rewrite of proof bar draw code

--- src/org/proofpad/ProofBar.java
+++ src/org/proofpad/ProofBar.java
@@ -14,7 +14,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.awt.geom.RoundRectangle2D;
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
@@ -34,9 +36,100 @@ public class ProofBar extends JComponent {
 	
 	static final Cursor HAND = new Cursor(Cursor.HAND_CURSOR);
 
+	private final class PBMouseListener extends MouseAdapter {
+		private final Acl2 acl2;
+		private final MoreBar mb;
+
+		PBMouseListener(Acl2 acl2, MoreBar mb) {
+			this.acl2 = acl2;
+			this.mb = mb;
+		}
+
+		@Override public void mouseClicked(MouseEvent e) {
+			if (e.getButton() != MouseEvent.BUTTON1) {
+				return;
+			}
+			if (numProving > 0) {
+				return;
+			}
+			int begin = 0;
+			int provedSoFar = numProved;
+			int addToNextHeight = 0;
+			int admissionIndexSoFar = acl2.numInitExps;
+			int i = 0;
+			for (Expression ex : expressions) {
+				int height = pixelHeight(ex);
+				height += addToNextHeight;
+				addToNextHeight = 0;
+				if (provedSoFar > 0) {
+					// Going through expressions that have been proven. A
+					// click here means we need to undo.
+					int admissionIndex = admissionIndices.get(i);
+					provedSoFar--;
+					if (provedSoFar == 0) {
+						addToNextHeight = ex.nextGapHeight * lineHeight / 2 - 1;
+						height -= addToNextHeight;
+					}
+					if (e.getY() < begin + height) {
+						for (; admissionIndexSoFar < admissionIndex; admissionIndexSoFar++) {
+							acl2.undo();
+						}
+						data.set(ex.expNum, null);
+						mb.repaint();
+						numProved--;
+						setReadOnlyIndex(Math.min(getReadOnlyIndex(),
+								ex.prev == null ? -1 : ex.prev.nextIndex - 1));
+						admissionIndices.remove(i);
+						i--;
+					}
+					i++;
+					if (admissionIndex > admissionIndexSoFar) {
+						admissionIndexSoFar = admissionIndex;
+					}
+				} else {
+					// A click here means we need to admit some new forms
+					if (error) {
+						break;
+					}
+					if (e.getY() > begin && ex.firstType != ExpType.FINAL) {
+						proofQueue.add(ex);
+						numProving++;
+						setReadOnlyIndex(Math.max(getReadOnlyIndex(), ex.nextIndex - 1));
+					}
+				}
+				begin += height;
+			}
+			toLogicMode();
+			proveNext();
+			error = false;
+			generateSegments();
+		}
+
+		@Override public void mouseEntered(MouseEvent e) {
+			hover = true;
+			repaint();
+		}
+
+		@Override public void mouseExited(MouseEvent e) {
+			hover = false;
+			repaint();
+		}
+	}
+
 	public interface ReadOnlyIndexChangeListener {
 		void readOnlyIndexChanged(int newIndex);
 	}
+	
+	public class PBSegment {
+		public Color fill = Color.WHITE;
+		public boolean proved;
+		public int begin;
+		public int height;
+		public Image icon;
+		public String tooltip = "";
+	}
+	
+	private final ArrayList<PBSegment> segments = new ArrayList<PBSegment>();
 	
 	public class ExpData {
 		Expression exp;
@@ -118,7 +211,6 @@ public class ProofBar extends JComponent {
 	private final PPWindow window;
 
 	public ProofBar(final Acl2 acl2, final MoreBar mb, final Acl2Parser parser, final PPWindow window) {
-		super();
 		this.acl2 = acl2;
 		this.mb = mb;
 		this.parser = parser;
@@ -130,80 +222,12 @@ public class ProofBar extends JComponent {
 				numProved = 0;
 				numProving = 0;
 				admissionIndices.clear();
-				repaint();
+				generateSegments();
 			}
 		});
 		setPreferredSize(new Dimension(width, 0));
-		setBorder(BorderFactory.createMatteBorder(0, 0, 0, 1, Color.LIGHT_GRAY));
-		addMouseListener(new MouseAdapter() {
-			@Override public void mouseClicked(MouseEvent e) {
-				if (e.getButton() != MouseEvent.BUTTON1) {
-					return;
-				}
-				if (numProving > 0) {
-					return;
-				}
-				int begin = 0;
-				int provedSoFar = numProved;
-				int addToNextHeight = 0;
-				int admissionIndexSoFar = acl2.numInitExps;
-				int i = 0;
-				for (Expression ex : expressions) {
-					int height = pixelHeight(ex);
-					height += addToNextHeight;
-					addToNextHeight = 0;
-					if (provedSoFar > 0) {
-						// Going through expressions that have been proven. A
-						// click here means we need to undo.
-						int admissionIndex = admissionIndices.get(i);
-						provedSoFar--;
-						if (provedSoFar == 0) {
-							addToNextHeight = ex.nextGapHeight * lineHeight / 2 - 1;
-							height -= addToNextHeight;
-						}
-						if (e.getY() < begin + height) {
-							for (; admissionIndexSoFar < admissionIndex; admissionIndexSoFar++) {
-								acl2.undo();
-							}
-							data.set(ex.expNum, null);
-							mb.repaint();
-							numProved--;
-							setReadOnlyIndex(Math.min(getReadOnlyIndex(),
-									ex.prev == null ? -1 : ex.prev.nextIndex - 1));
-							admissionIndices.remove(i);
-							i--;
-						}
-						i++;
-						if (admissionIndex > admissionIndexSoFar) {
-							admissionIndexSoFar = admissionIndex;
-						}
-					} else {
-						// A click here means we need to admit some new forms
-						if (error) {
-							break;
-						}
-						if (e.getY() > begin && ex.firstType != ExpType.FINAL) {
-							proofQueue.add(ex);
-							numProving++;
-							setReadOnlyIndex(Math.max(getReadOnlyIndex(), ex.nextIndex - 1));
-						}
-					}
-					begin += height;
-				}
-				toLogicMode();
-				proveNext();
-				error = false;
-				repaint();
-			}
-			@Override public void mouseEntered(MouseEvent e) {
-				hover = true;
-				repaint();
-			}
-			@Override public void mouseExited(MouseEvent e) {
-				hover = false;
-				repaint();
-			}
-		});
+		setBorder(BorderFactory.createMatteBorder(0, 0, 0, 1, Color.GRAY));
+		addMouseListener(new PBMouseListener(acl2, mb));
 		addMouseMotionListener(new MouseMotionListener() {
 			@Override public void mouseDragged(MouseEvent e) {
 				mouseMoved(e);
@@ -230,14 +254,100 @@ public class ProofBar extends JComponent {
 		expressions = new Vector<Expression>();
 		numProved = 0;
 	}
-	
 
 	@Override protected void paintComponent(Graphics gOld) {
-		// TODO: Draw warnings
 		Graphics2D g = (Graphics2D) gOld;
+		// TODO: Draw only what's in clipBounds to scroll faster.
 		Rectangle clipBounds = g.getClipBounds();
 		int clipYMax = clipBounds.y + clipBounds.height + 10;
-		// TODO: Draw only what's in clipBounds to scroll faster.
+		setToolTipText("");
+		boolean calloutBelowShown = false;
+		boolean calloutAboveShown = false;
+		int roundDim = 5;
+		g.setColor(Color.GRAY);
+		g.draw(clipBounds);
+		try {
+			//for (PBSegment seg : segments) {
+			for (int i = segments.size() - 1; i >= 0; i--) {
+				PBSegment seg = segments.get(i);
+				if (seg.fill.equals(Color.WHITE)) {
+					continue;
+				}
+				int begin = seg.begin;
+				int height = seg.height;
+				g.setColor(seg.fill);
+				RoundRectangle2D.Double rect = new RoundRectangle2D.Double(0, begin - 10, 19,
+						height + 10, roundDim, roundDim);
+				g.fill(rect);
+				g.setColor(Color.GRAY);
+				g.draw(rect);
+				if (seg.icon != null) {
+					g.drawImage(seg.icon, (width - seg.icon.getWidth(this)) / 2,
+							(height - seg.icon.getHeight(this)) / 2 + begin, this);
+				}
+				int end = begin + height;
+				if (my >= begin && my < end) {
+					setToolTipText(seg.tooltip);
+				}
+				if (seg.fill == ERROR_COLOR) {
+					if (begin > clipBounds.y + clipBounds.height) {
+						window.showErrorCallout(true, new Rectangle(0, begin, 0, height));
+						calloutBelowShown = true;
+					} else if (end < clipBounds.y) {
+						window.showErrorCallout(false, new Rectangle(0, begin, 0, height));
+						calloutAboveShown = true;
+					}
+				}
+				if (hover && numProving == 0) {
+					if (seg.proved && my <= end) {
+						g.setColor(UNTRIED_COLOR);
+						rect = new RoundRectangle2D.Double(0, begin - 10 + (my > begin ? 20 : 0), 19,
+								height + 10 - (my > begin ? 20 : 0), roundDim, roundDim);
+						g.fill(rect);
+						g.setColor(Color.GRAY);
+						g.draw(rect);
+						g.setColor(UNTRIED_COLOR);
+						setToolTipText("Undo admitting this term.");
+						if (my > begin) {
+							g.fillPolygon(new int[] {1, 1, width / 2, width - 1, width - 1},
+										  new int[] {begin + 20, begin + 9, begin, begin + 9, begin + 20},
+										  5);
+						}
+					}
+					if (!seg.proved && my >= begin) {
+						g.setColor(PROVED_COLOR);
+						rect = new RoundRectangle2D.Double(0, begin - 10, 19, height + 10 -
+								(my < end ? 10 : 0), roundDim, roundDim);
+						g.fill(rect);
+						g.setColor(Color.GRAY);
+						g.draw(rect);
+						g.setColor(PROVED_COLOR);
+						if (my < end) {
+							g.fillPolygon(new int[] {1, 1, width / 2, width - 1, width - 1},
+									  new int[] {end - 20, end - 9, end, end - 9, end - 20},
+									  5);
+							setToolTipText("Admit this term.");
+						}
+					}
+				}
+//				g.setColor(Color.GRAY);
+//				g.drawLine(0, begin, width, begin);
+				if (!calloutAboveShown) {
+					window.hideErrorCallout(false);
+				}
+				if (!calloutBelowShown) {
+					window.hideErrorCallout(true);
+				}
+				setPreferredSize(new Dimension(clipBounds.x + clipBounds.width, clipYMax));
+			}
+		} catch (IndexOutOfBoundsException e) {
+			repaint();
+		}
+	}
+
+	void generateSegments() {
+		segments.clear();
+		// TODO: Draw warnings
 		int begin = 0;
 		int provedSoFar = numProved;
 		int provingSoFar = numProving;
@@ -248,99 +358,58 @@ public class ProofBar extends JComponent {
 		}
 		int unprovenIdx = 0;
 		Status expStatus = Status.UNTRIED;
-		boolean calloutBelowShown = false;
-		boolean calloutAboveShown = false;
 		for (Expression e: expressions) {
-			int height = pixelHeight(e);
-			height += addToNextHeight;
+			PBSegment seg = new PBSegment();
+			seg.height = pixelHeight(e);
+			seg.height += addToNextHeight;
 			addToNextHeight = 0;
+			seg.begin = begin;
 			if (provedSoFar == 0 && unprovenStates.size() > unprovenIdx) {
 				expStatus = unprovenStates.get(unprovenIdx).status;
 				unprovenIdx++;
 			}
 			if (provedSoFar > 0) {
-				// Drawing proved terms
+				seg.proved = true;
 				provedSoFar--;
 				if (provedSoFar == 0) {
 					addToNextHeight = e.nextGapHeight * lineHeight / 2 - 1;
-					height -= addToNextHeight;
-					setReadOnlyHeight(begin + height);
+					seg.height -= addToNextHeight;
+					setReadOnlyHeight(begin + seg.height);
 				}
-				if (hover && my < begin + height) {
-//					g.setPaint(unprove);
-					g.setColor(UNTRIED_COLOR);
-					g.fillRect(0, begin, 30, height);
-					if (my >= begin) {
-						g.setColor(PROVED_COLOR);
-						g.fillPolygon(new int[] {0, width / 2, width, width, 0, 0},
-								new int[] {begin + 10, begin, begin + 10, begin, begin, begin + 10},
-								6);
-						setToolTipText("Undo admitting this term.");
-					}
-				} else {
-					g.setColor(PROVED_COLOR);
-					g.fillRect(0, begin, 30, height);
-					g.drawImage(successIcon.getImage(), (width - 19) / 2, (height - 19) / 2 + begin, this);
-				}
+				seg.fill = PROVED_COLOR;
+				seg.icon = successIcon.getImage();
 			} else if ((isError && provedSoFar == 0) || expStatus == Status.FAILURE) {
-				// Draw error box
+				unprovenIdx++;
 				isError = false;
-				g.setColor(ERROR_COLOR);
-				g.fillRect(0, begin, 30, height);
-				if (hover && my > begin && my <= begin + height) {
-					setToolTipText("An error occured. See the log below for details.");
-				}
-				if (begin > clipBounds.y + clipBounds.height) {
-					window.showErrorCallout(true, new Rectangle(0, begin, 0, height));
-					calloutBelowShown = true;
-				} else if (begin + height < clipBounds.y) {
-					window.showErrorCallout(false, new Rectangle(0, begin, 0, height));
-					calloutAboveShown = true;
-				}
-				g.drawImage(errorIcon.getImage(), (width - 19) / 2, (height - 19) / 2 + begin, this); 
+				seg.proved = false;
+				seg.fill = ERROR_COLOR;
+				seg.icon = errorIcon.getImage();
+				seg.tooltip = "An error occured. See the log below for details.";
 			} else if (provingSoFar > 0) {
-				// Drawing in-progress terms
+				seg.proved = true;
 				provingSoFar--;
 				if (provingSoFar == 0) {
-					setReadOnlyHeight(begin + height - e.nextGapHeight * lineHeight / 2);
+					setReadOnlyHeight(begin + seg.height - e.nextGapHeight * lineHeight / 2);
 				}
-				g.setColor(IN_PROGRESS_COLOR);
-				g.fillRect(0, begin, 30, height);
-				g.drawImage(inProgressThrobber, (width - 16) / 2, begin + (height - 16) / 2, this);
+				seg.fill = IN_PROGRESS_COLOR;
+				seg.icon = inProgressThrobber;
 			} else if (e.firstType != SExpUtils.ExpType.FINAL) {
 				// Drawing untried or admitted terms
-				Color expColor = expStatus == Status.SUCCESS ? ADMITTED_COLOR : UNTRIED_COLOR; 
-				if (hover && my > begin && !error && !e.contents.equals("")) {
-					g.setColor(PROVED_COLOR);
-					g.fillRect(0, begin, 30, height);
-					if (my <= begin + height) {
-						g.setColor(expColor);
-						int yEnd = begin + height;
-						g.fillPolygon(new int[] {0, width / 2, width, width, 0, 0},
-								new int[] {yEnd - 10, yEnd, yEnd - 10, yEnd, yEnd, yEnd - 10},
-								6);
-						setToolTipText("Admit this term.");
-					}
-				} else {
-					g.setColor(expColor);
-					g.fillRect(0, begin, 30, height);
-					if (expStatus == Status.UNTRIED && isAdmitting) {
-						g.drawImage(admittingThrobber, (width - 16) / 2, begin + (height - 16) / 2,
-								this);
-					}
+				seg.proved = false;
+				Color expColor = expStatus == Status.SUCCESS ? ADMITTED_COLOR : UNTRIED_COLOR;
+				seg.fill = expColor;
+				if (expStatus == Status.UNTRIED && isAdmitting) {
+					seg.icon = admittingThrobber;
 				}
 			}
-			g.setColor(Color.GRAY);
-			g.drawLine(0, begin, width, begin);
-			begin += height;
+			begin += seg.height;
+			if (e.contents.isEmpty()) {
+				seg.fill = Color.WHITE;
+				seg.icon = null;
+			}
+			segments.add(seg);
 		}
-		if (!calloutAboveShown) {
-			window.hideErrorCallout(false);
-		}
-		if (!calloutBelowShown) {
-			window.hideErrorCallout(true);
-		}
-		setPreferredSize(new Dimension(clipBounds.x + clipBounds.width, clipYMax));
+		repaint();
 	}
 
 	private void setReadOnlyHeight(int newHeight) {
@@ -363,7 +432,7 @@ public class ProofBar extends JComponent {
 
 	public void setLineHeight(int lineHeight) {
 		this.lineHeight = lineHeight;
-		repaint();
+		generateSegments();
 	}
 	
 	public void adjustHeights(java.util.LinkedList<Expression> newExps) {
@@ -385,7 +454,7 @@ public class ProofBar extends JComponent {
 		for (UnprovenExp e : unprovenStates) {
 			e.status = Status.UNTRIED;
 		}
-		repaint();
+		generateSegments();
 	}
 
 	private void clearProgramModeData() {
@@ -415,13 +484,13 @@ public class ProofBar extends JComponent {
 			} else {
 				admitUnprovenExps();
 			}
-			repaint();
+			generateSegments();
 		} else if (!success) {
 			numProving = 0;
 			setReadOnlyIndex(tried.prev == null ? -1 : tried.prev.nextIndex - 1);
 			proofQueue.clear();
 			error = true;
-			repaint();
+			generateSegments();
 		}
 	}
 	
@@ -447,14 +516,14 @@ public class ProofBar extends JComponent {
 		if (proofQueue.size() == 0) {
 			numProving = 0;
 			admitUnprovenExps();
-			repaint();
+			generateSegments();
 			return;
 		}
 		tried = proofQueue.remove(0);
 		if (tried.contents.equals("")) {
 			tried = null;
 			numProving = 0;
-			repaint();
+			generateSegments();
 			return;
 		}
 		final boolean last = proofQueue.isEmpty();
@@ -498,7 +567,7 @@ public class ProofBar extends JComponent {
 				break;
 			}
 		}
-		repaint();
+		generateSegments();
 	}
 	
 	public void undoOneItem() {
@@ -526,7 +595,7 @@ public class ProofBar extends JComponent {
 			}
 			i++;
 		}
-		repaint();
+		generateSegments();
 	}
 
 	public void resetProgress() {
@@ -548,7 +617,7 @@ public class ProofBar extends JComponent {
 
 	public void setReadOnlyIndex(int readOnlyIndex) {
 
-		repaint();
+		generateSegments();
 		if (this.readOnlyIndex != readOnlyIndex) {
 			fireReadOnlyIndexChange(readOnlyIndex);
 		}
@@ -570,7 +639,7 @@ public class ProofBar extends JComponent {
 		if (numProving > 0) return;
 		int provedSoFar = numProved;
 		isAdmitting = true;
-		repaint();
+		generateSegments();
 		acl2.admit(":program", Acl2.doNothingCallback);
 		acl2.admit("(set-ld-redefinition-action '(:doit . :erase) state)", Acl2.doNothingCallback);
 		if (unprovenStates == null) unprovenStates = new ArrayList<UnprovenExp>();
@@ -601,8 +670,8 @@ public class ProofBar extends JComponent {
 							alreadyShownAnError = false;
 							isAdmitting = false;
 						}
-						repaint();
 						ue.status = success ? Status.SUCCESS : Status.FAILURE;
+						generateSegments();
 						return false;
 					}
 				});
@@ -618,6 +687,8 @@ public class ProofBar extends JComponent {
 			data.subList(exp.expNum, data.size() - 1).clear();
 		} catch (IndexOutOfBoundsException e) {
 			return;
+		} catch (ConcurrentModificationException e) {
+			setExpData(exp, success, response, last);
 		}
 		ExpData expData = new ExpData(exp, response, success ? MsgType.SUCCESS : MsgType.ERROR);
 		while (data.size() <= exp.expNum) {
